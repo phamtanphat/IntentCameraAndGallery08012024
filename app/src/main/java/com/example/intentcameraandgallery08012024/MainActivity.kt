97b75ec6d8959d199fa591b6b54493f9d430c992
phatdroid94 <phatdroid94@gmail.com>
2024-07-22 19:00:42 +0700

    request permission

--- app/src/main/java/com/example/intentcameraandgallery08012024/MainActivity.kt
+++ app/src/main/java/com/example/intentcameraandgallery08012024/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.intentcameraandgallery08012024
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnCamera: Button
+    private lateinit var btnGallery: Button
+    private var REQUEST_CODE_CAMERA = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +26,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnGallery = findViewById(R.id.buttonGallery)
+        btnCamera = findViewById(R.id.buttonCamera)
+
+        btnCamera.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_CODE_CAMERA
+                )
+            }
         }
     }
 }
